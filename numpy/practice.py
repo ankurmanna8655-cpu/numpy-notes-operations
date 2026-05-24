87a6c233bf2af250e3e5e7ce9254556094b9cd3a
Ankur <ankurmanna8655@gmail.com>
2026-05-24 13:56:49 +0530

    second day update 3 questions addes

--- numpy/practice.py
+++ numpy/practice.py
@@ -68,3 +68,72 @@ print("Count of elements \n",count)
 for elements,counts in zip(unique_elements,count):
     print(f"{elements} occurs {counts} times")
 
+# 5. Mini Marks Analysis
+
+# Create marks of 5 students in 3 subjects using NumPy.
+
+# Find:
+
+# average marks per student
+# topper
+# subject-wise highest marks
+
+marks = np.array([
+    [85, 90, 78],
+    [88, 76, 92],
+    [90, 91, 89],
+    [70, 65, 80],
+    [95, 88, 84]
+])
+
+#1.
+print("Average marks per student is :",np.mean(marks,axis =1))
+
+#2.
+topper_marks = np.sum(marks,axis =1)
+print("Total Marks of each student is :",topper_marks)
+
+topper_index = np.argmax(topper_marks)
+print(f"Topper is student {topper_index+1} ")
+
+#3.
+print("Subjectwise highest marks is :",np.max(marks,axis=0))
+
+#6. Create a border of 1s around a matrix of 0s.
+
+# Example:
+
+# 1 1 1 1 1
+# 1 0 0 0 1
+# 1 0 0 0 1
+# 1 1 1 1 1
+
+new_matrix = np.ones((4,5))
+
+new_matrix[1:-1,1:-1] = 0      
+#  arr[1:-1, 1:-1] ows from index 1 to second-last columns from index 1 to second-last
+print(new_matrix)
+
+# 7. Given daily temperatures:
+
+# temps = np.array([30,32,31,29,35,36,34])
+
+# Find:
+
+# hottest day
+# coldest day
+# days above average
+
+temps = np.array([30,32,31,29,35,36,34])
+hottest_temp = np.max(temps)
+hottest_day = np.argmax(temps)
+print(f"Hottest is DAY: {hottest_day+1} and hottest temp is {hottest_temp}")
+
+coldest_temp = np.min(temps)
+coldest_day = np.argmin(temps)
+print(f"Coldest is DAY: {coldest_day+1} and coldest temp is {coldest_temp}")
+
+avg_temp = np.mean(temps)
+above_avg = temps > avg_temp
+above_avg_index = np.argmax(temps)
+print(f"Temperatures above averages are :{temps[above_avg]} ")
